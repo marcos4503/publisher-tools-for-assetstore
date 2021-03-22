2d5e6c5cbcbaf364939fb84b6d5173d22d5adf37
Marcos Tomaz <marcos1tomaz2@gmail.com>
2021-03-22 20:16:09 -0300

    Background service completed

--- Publisher-Tools-For-Asset-Store/js/background.js
+++ Publisher-Tools-For-Asset-Store/js/background.js
@@ -105,7 +105,45 @@ function OnRunNewUpdateOfService() {
                 problemsOcurredOnDoSalesRequest = false;
                 ShowOrHideBadgeIfOcurredErrorOnSomeRequest();
 
-                console.log("sales done");
+                //Allocate variables of sales information
+                var lastCheckTotalSales = 0;
+                var currentCheckTotalSales = 0;
+
+                //Process JSON returned Data
+                var responseJson = JSON.parse(httpSalesRequest.responseText);
+                var allProducts = responseJson.aaData;
+                var allResults = responseJson.result;
+                for (var i = 0; i < allProducts.length; i++) {
+                    var productName = allProducts[i][0];
+                    var productPrice = allProducts[i][1];
+                    var productSales = allProducts[i][2];
+                    var productRefunds = allProducts[i][3];
+                    var productChargebacks = allProducts[i][4];
+                    var productGrossSales = allProducts[i][5];
+                    var productFirstSale = allProducts[i][6];
+                    var productLastSale = allProducts[i][7];
+                    var productNetSales = allResults[i].net;
+                    var productShortUrl = allResults[i].short_url;
+
+                    //If have sales data for this asset, compare and notify
+                    if (localStorage.getItem(productName + " (Sales)") != null)
+                        lastCheckTotalSales += parseInt(JSON.parse(localStorage.getItem(productName + " (Sales)")).lastSalesCount, 10);
+                    currentCheckTotalSales += parseInt(productSales, 10);
+
+                    //Save new sales data for this asset
+                    localStorage.setItem(productName + " (Sales)", '{"lastSalesCount":"' + productSales + '", "lastCheckMonth":"' + mm + '"}');
+                }
+
+                //If currentCheckTotalSales is greater than lastCheckTotalSales, notify publisher about new sales
+                if (currentCheckTotalSales > lastCheckTotalSales)
+                    chrome.notifications.create("",
+                        {
+                            type: "basic",
+                            iconUrl: "../img/new-sales.png",
+                            title: "New Sales Made!",
+                            message: (currentCheckTotalSales - lastCheckTotalSales).toString() + " new sales have been made since the last check done, in this Month."
+                        },
+                        function () { });
             }
             //On fail
             if (this.status != 200) {
@@ -132,7 +170,92 @@ function OnRunNewUpdateOfService() {
                 problemsOcurredOnDoReviewsRequest = false;
                 ShowOrHideBadgeIfOcurredErrorOnSomeRequest();
 
-                console.log("reviews done");
+                //Allocate variables of sales information
+                var foundNewReviewPosted = false;
+                var notificationContentForNewReviewPosted = "";
+                var foundNewReviewUpdated = false;
+                var notificationContentForNewReviewUpdated = "";
+
+                //Process XML RSS returned Data
+                var xmlDoc = httpReviewsRequest.responseXML;
+                var rssRootNode = xmlDoc.getElementsByTagName("rss");
+                var reviewsNodes = rssRootNode[0].getElementsByTagName("item");
+                //Loop to find last New Review
+                for (var i = 0; i < reviewsNodes.length; i++) {
+                    //Get title of this review
+                    var titleNodeText = reviewsNodes[i].getElementsByTagName("title")[0].innerHTML;
+                    var descriptionNodeText = reviewsNodes[i].getElementsByTagName("description")[0].innerHTML;
+                    //If this is a reply from publisher, ignore this and move to next
+                    if (titleNodeText.includes("New reply") == true)
+                        continue;
+                    //If this is a new review from custumer
+                    if (titleNodeText.includes("New review") == true) {
+                        //If have review data of last new review, compare and notify
+                        var lastCheckLastNewReviewTitle = "";
+                        if (localStorage.getItem("lastNewReviewPosted") != null)
+                            lastCheckLastNewReviewTitle = JSON.parse(localStorage.getItem("lastNewReviewPosted")).description;
+                        var currentCheckLastNewReviewTitle = encodeURI(descriptionNodeText);
+
+                        //Save last new review posted data
+                        localStorage.setItem("lastNewReviewPosted", '{"assetName":"' + titleNodeText.split("\"")[1] + '", "author":"' + titleNodeText.split("\"  by ")[1] + '", "description":"' + encodeURI(descriptionNodeText) + '", "stars":"' + ((descriptionNodeText.match(/&amp;#9733;/g) || []).length).toString() + '"}');
+
+                        //Inform the new review, if currentCheckLastNewReviewTitle is differente from lastCheckLastNewReviewTitle
+                        if (currentCheckLastNewReviewTitle != lastCheckLastNewReviewTitle) {
+                            foundNewReviewPosted = true;
+                            notificationContentForNewReviewPosted = "A new Review was Posted for asset \"" + JSON.parse(localStorage.getItem("lastNewReviewPosted")).assetName + "\" since the last check.";
+                        }
+                        break;
+                    }
+                }
+                //Loop to find last Updated Review
+                for (var i = 0; i < reviewsNodes.length; i++) {
+                    //Get title of this review
+                    var titleNodeText = reviewsNodes[i].getElementsByTagName("title")[0].innerHTML;
+                    var descriptionNodeText = reviewsNodes[i].getElementsByTagName("description")[0].innerHTML;
+                    //If this is a reply from publisher, ignore this and move to next
+                    if (titleNodeText.includes("New reply") == true)
+                        continue;
+                    //If this is a updated review from custumer
+                    if (titleNodeText.includes("Updated review") == true) {
+                        //If have review data of last updated review, compare and notify
+                        var lastCheckLastUpdatedReviewTitle = "";
+                        if (localStorage.getItem("lastNewReviewUpdated") != null)
+                            lastCheckLastUpdatedReviewTitle = JSON.parse(localStorage.getItem("lastNewReviewUpdated")).description;
+                        var currentCheckLastUpdatedReviewTitle = encodeURI(descriptionNodeText);
+
+                        //Save last new review posted data
+                        localStorage.setItem("lastNewReviewUpdated", '{"assetName":"' + titleNodeText.split("\"")[1] + '", "author":"' + titleNodeText.split("\"  by ")[1] + '", "description":"' + encodeURI(descriptionNodeText) + '", "stars":"' + ((descriptionNodeText.match(/&amp;#9733;/g) || []).length).toString() + '"}');
+
+                        //Inform the new review, if currentCheckLastUpdatedReviewTitle is differente from lastCheckLastUpdatedReviewTitle
+                        if (currentCheckLastUpdatedReviewTitle != lastCheckLastUpdatedReviewTitle) {
+                            foundNewReviewUpdated = true;
+                            notificationContentForNewReviewUpdated = "A Review was Updated in asset \"" + JSON.parse(localStorage.getItem("lastNewReviewUpdated")).assetName + "\" since the last check.";
+                        }
+                        break;
+                    }
+                }
+
+                //If found new review posted
+                if (foundNewReviewPosted == true)
+                    chrome.notifications.create("",
+                        {
+                            type: "basic",
+                            iconUrl: "../img/new-review.png",
+                            title: "New Review Posted!",
+                            message: notificationContentForNewReviewPosted
+                        },
+                        function () { });
+
+                //If found new review updated
+                if (foundNewReviewUpdated == true)
+                    chrome.notifications.create("",
+                        {
+                            type: "basic",
+                            iconUrl: "../img/updated-review.png",
+                            title: "New Review Updated!",
+                            message: notificationContentForNewReviewUpdated
+                        },
+                        function () { });
             }
             //On fail
             if (this.status != 200) {
@@ -159,52 +282,3 @@ function ShowOrHideBadgeIfOcurredErrorOnSomeRequest() {
     if (problemsOcurredOnDoSalesRequest == false && problemsOcurredOnDoReviewsRequest == false)
         chrome.browserAction.setBadgeText({ text: '' });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-//----- Important functions ------
-//Function that post process the response returned from sales API in GetSalesAndNotifyIfHaveNew
-function ProcessResponseFromUnitySalesAPI(responseText) {
-    //Get converted data in json
-    var jsonData = JSON.parse(responseText);
-    //Get current date
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0');
-    var time = today.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-
-    //Process sales data for each product
-    var allProducts = jsonData.aaData;
-    var allResults = jsonData.result;
-    for (var i = 0; i < allProducts.length; i++) {
-        var productName = allProducts[i][0];
-        var productPrice = allProducts[i][1];
-        var productSales = allProducts[i][2];
-        var productRefunds = allProducts[i][3];
-        var productChargebacks = allProducts[i][4];
-        var productGrossSales = allProducts[i][5];
-        var productFirstSale = allProducts[i][6];
-        var productLastSale = allProducts[i][7];
-        var productNetSales = allResults[i].net;
-        var productShortUrl = allResults[i].short_url;
-
-        //Save data for this asset
-        localStorage.setItem(productName + " (Sales)", '{"lastSales":"' + productSales + '", "lastCheck":"' + dd + "/" + mm + "-" + time + '"}');
-    }
-}*/
