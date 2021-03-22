293aa46a71716cc7dcdd438cb1512e5528160f43
Marcos Tomaz <marcos1tomaz2@gmail.com>
2021-03-22 17:23:01 -0300

    Beta version

--- Publisher-Tools-For-Asset-Store/js/background.js
+++ Publisher-Tools-For-Asset-Store/js/background.js
@@ -1,86 +1,184 @@
 // @ts-nocheck
 
 //Private variables
-var id = 0;
+var currentAlarmsRunnedCount = 0;
+var problemsOcurredOnDoSalesRequest = false;
+var problemsOcurredOnDoReviewsRequest = false;
 
 //----- Starting functions ------
 
-//Start in background, calling all needed important functions
-setTimeout(function () { CallAllNeededImportantFunctions(); }, 1000);
-//Create the alarm for call all needed important functions. Alarm will repeat every X minutes
-chrome.alarms.create("CallAllNeededImportantFunctions", {
-    delayInMinutes: 1,
-    periodInMinutes: 1
-});
-chrome.alarms.onAlarm.addListener(function (alarm) {
-    if (alarm.name === "CallAllNeededImportantFunctions") {
-        CallAllNeededImportantFunctions();
-    }
-});
-//Create a message receiver for CallAllNeededImportantFunctions inside of popup
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.msg == "CallAllNeededImportantFunctions")
-            CallAllNeededImportantFunctions();
-    }
-);
+//Function that start the background service
+function StartBackgroundService() {
+    //Get current date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var time = today.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
 
-//----- Core functions ------
+    //Create the basics settings, if not have yet
+    if (localStorage.getItem("delayBetweenUpdates") == null)
+        localStorage.setItem("delayBetweenUpdates", "30");
+    if (localStorage.getItem("showNotificationOnStart") == null)
+        localStorage.setItem("showNotificationOnStart", "false");
 
-//Main function that calls all others needed
-function CallAllNeededImportantFunctions() {
-    //Show badge of error if not provided review rss or publisher id
-    if (localStorage.getItem("publisherId") == null || localStorage.getItem("reviewsRss") == null)
-        SetVisibilityOfBadgeInExtensionIcon(true);
+    //Notify the start, if is enabled
+    if (localStorage.getItem("showNotificationOnStart") == "true")
+        chrome.notifications.create("",
+            {
+                type: "basic",
+                iconUrl: "../img/icon128.png",
+                title: "Publisher Tools For Asset Store",
+                message: "The Background Service of this Extension is running!"
+            },
+            function () { });
 
-    //Check for new sales (if provided publisher ID)
-    if (localStorage.getItem("publisherId") != null)
-        GetSalesAndNotifyIfHaveNew();
+    //Register a new alarm to run periodics updates, register the receiver too
+    chrome.alarms.create("OnRunNewUpdateOfService", {
+        delayInMinutes: 1,
+        periodInMinutes: 1
+    });
+    chrome.alarms.onAlarm.addListener(function (alarm) {
+        if (alarm.name === "OnRunNewUpdateOfService") {
+            //If alarm count is in the desired delay between updates, run the update of service and reset counter
+            if (currentAlarmsRunnedCount >= parseInt(localStorage.getItem("delayBetweenUpdates"), 10)) {
+                OnRunNewUpdateOfService();
 
-    //Send message to popup update all content
-    chrome.runtime.sendMessage({ msg: "UpdateAllContentOfPopUp" });
+                //Reset the currentAlarmsRunnedCount
+                currentAlarmsRunnedCount = 0;
+            }
+
+            //Increase runs of alarm count
+            currentAlarmsRunnedCount += 1;
+        }
+    });
+
+    //Register a lister for receive all types of messages
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            if (request.msg == "StartBackgroundService")
+                StartBackgroundService();
+
+            if (request.msg == "OnRunNewUpdateOfService")
+                OnRunNewUpdateOfService();
+        }
+    );
+
+    //Set default color for badges of this extension
+    chrome.browserAction.setBadgeBackgroundColor({ color: [190, 0, 0, 255] });
+
+    //Call OnRunNewUpdateOfService to run first update
+    OnRunNewUpdateOfService();
+
+    //Notify this start on console
+    console.log("The Service Was Started In " + dd + "/" + mm + " " + time + ".");
 }
-//Function that shows or hide the badge in extension icon
-function SetVisibilityOfBadgeInExtensionIcon(show) {
-    //If is desired to show
-    if (show == true) {
-        chrome.browserAction.setBadgeBackgroundColor({ color: [190, 0, 0, 255] });
+
+//Start the background service
+StartBackgroundService();
+
+//------- Core function ---------
+
+//Function that runs on each update of service
+function OnRunNewUpdateOfService() {
+    //Get current date
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yy = today.getFullYear();
+    var time = today.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+
+    //If publisher not provided needed information yet, cancel this update
+    if (localStorage.getItem("publisherId") == null || localStorage.getItem("reviewsRss") == null) {
         chrome.browserAction.setBadgeText({ text: 'Login' });
+        return;
     }
-    //If is not desired to show
-    if (show == false) {
-        chrome.browserAction.setBadgeBackgroundColor({ color: [190, 0, 0, 255] });
-        chrome.browserAction.setBadgeText({ text: '' });
-    }
-}
 
-//----- Important functions ------
-
-//Function to check for new sales and notify if necessary
-function GetSalesAndNotifyIfHaveNew() {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
+    //-------------- Do a HTTP Request to process Sales -------------
+    var httpSalesRequest = new XMLHttpRequest();
+    httpSalesRequest.onreadystatechange = function () {
         //On done loading
         if (this.readyState == 4) {
             //On success
             if (this.status == 200) {
-                //Post processes response returned
-                ProcessResponseFromUnitySalesAPI(httpRequest.responseText);
+                //Notify the success to the PopUp interface and the Badge of extension
+                chrome.runtime.sendMessage({ msg: "ReportDoneLoadingSalesData", errorOcurred: "false" });
+                problemsOcurredOnDoSalesRequest = false;
+                ShowOrHideBadgeIfOcurredErrorOnSomeRequest();
 
-                //Remove the warning from icon
-                SetVisibilityOfBadgeInExtensionIcon(false);
+                console.log("sales done");
             }
             //On fail
             if (this.status != 200) {
-                //Add the warning from icon
-                SetVisibilityOfBadgeInExtensionIcon(true);
+                //Notify the fail to the PopUp interface and the Badge of extension
+                chrome.runtime.sendMessage({ msg: "ReportDoneLoadingSalesData", errorOcurred: "true" });
+                problemsOcurredOnDoSalesRequest = true;
+                ShowOrHideBadgeIfOcurredErrorOnSomeRequest();
             }
         }
     };
-    httpRequest.open("GET", "https://publisher.assetstore.unity3d.com/api/publisher-info/sales/40306/202103.json", true);
-    httpRequest.withCredentials = true;
-    httpRequest.send();
+    httpSalesRequest.open("GET", "https://publisher.assetstore.unity3d.com/api/publisher-info/sales/" + localStorage.getItem("publisherId") + "/" + yy + mm + ".json", true);
+    httpSalesRequest.withCredentials = true;
+    httpSalesRequest.send();
+
+    //-------------- Do a HTTP Request to process Reviews -------------
+    var httpReviewsRequest = new XMLHttpRequest();
+    httpReviewsRequest.onreadystatechange = function () {
+        //On done loading
+        if (this.readyState == 4) {
+            //On success
+            if (this.status == 200) {
+                //Notify the success to the PopUp interface and the Badge of extension
+                chrome.runtime.sendMessage({ msg: "ReportDoneLoadingReviewsData", errorOcurred: "false" });
+                problemsOcurredOnDoReviewsRequest = false;
+                ShowOrHideBadgeIfOcurredErrorOnSomeRequest();
+
+                console.log("reviews done");
+            }
+            //On fail
+            if (this.status != 200) {
+                //Notify the fail to the PopUp interface and the Badge of extension
+                chrome.runtime.sendMessage({ msg: "ReportDoneLoadingReviewsData", errorOcurred: "true" });
+                problemsOcurredOnDoReviewsRequest = true;
+                ShowOrHideBadgeIfOcurredErrorOnSomeRequest();
+            }
+        }
+    };
+    httpReviewsRequest.open("GET", localStorage.getItem("reviewsRss"), true);
+    httpReviewsRequest.withCredentials = true;
+    httpReviewsRequest.send();
+
+    //Notify this update on console
+    console.log("New Update Of Service Was Runned In " + dd + "/" + mm + " " + time + ".");
 }
+
+//Function that show or hide the badge, if ocurred problems on some http request
+function ShowOrHideBadgeIfOcurredErrorOnSomeRequest() {
+    //Show or hide the badge, if ocurred problems on some Http request
+    if (problemsOcurredOnDoSalesRequest == true || problemsOcurredOnDoReviewsRequest == true)
+        chrome.browserAction.setBadgeText({ text: 'Error' });
+    if (problemsOcurredOnDoSalesRequest == false && problemsOcurredOnDoReviewsRequest == false)
+        chrome.browserAction.setBadgeText({ text: '' });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//----- Important functions ------
 //Function that post process the response returned from sales API in GetSalesAndNotifyIfHaveNew
 function ProcessResponseFromUnitySalesAPI(responseText) {
     //Get converted data in json
@@ -109,4 +207,4 @@ function ProcessResponseFromUnitySalesAPI(responseText) {
         //Save data for this asset
         localStorage.setItem(productName + " (Sales)", '{"lastSales":"' + productSales + '", "lastCheck":"' + dd + "/" + mm + "-" + time + '"}');
     }
-}
+}*/
--- Publisher-Tools-For-Asset-Store/js/popup.js
+++ Publisher-Tools-For-Asset-Store/js/popup.js
@@ -1,57 +1,180 @@
 // @ts-nocheck
 
 //Private variables
-var id = 0;
+var doneLoadingSalesData = false;
+var doneLoadingReviewsData = false;
+var haveErrorOnGetSalesInfo = false;
+var haveErrorOnGetReviewsInfo = false;
 
-//----- Starting functions ------
-//Update all content of popup after window is loaded
-window.onload = function () {
-    UpdateAllContentOfPopUp();
-}
-//Create a message receiver for UpdateAllContentOfPopUp
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.msg == "UpdateAllContentOfPopUp")
-            UpdateAllContentOfPopUp();
+//------ Starting functions ------
+
+//Function that start popup service
+function StartPopUpService() {
+    //Register a lister for receive all types of messages
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            if (request.msg == "StartPopUpService")
+                StartPopUpService();
+
+            if (request.msg == "RenderOrUpdatePopUpContent")
+                RenderOrUpdatePopUpContent();
+
+            if (request.msg == "ReportDoneLoadingSalesData")
+                ReportDoneLoadingSalesData((request.errorOcurred == "true") ? true : false);
+
+            if (request.msg == "ReportDoneLoadingReviewsData")
+                ReportDoneLoadingReviewsData((request.errorOcurred == "true") ? true : false);
+        }
+    );
+
+    //Force service to run a new update that will inform this popup on end
+    chrome.runtime.sendMessage({ msg: "OnRunNewUpdateOfService" });
+
+    //Run the first renderer of popup content after created all HTML dom of popup
+    window.onload = function () {
+        RenderOrUpdatePopUpContent();
     }
-);
-//Call all important functions of background
-chrome.runtime.sendMessage({ msg: "CallAllNeededImportantFunctions" });
+}
 
-//----- Core functions ------
+//Start the popup service
+StartPopUpService();
 
-//Update all content of popup
-function UpdateAllContentOfPopUp() {
-    //Register handler for login button
-    document.getElementById("loginButton").addEventListener("click", function () { ValidateAndSaveDataOfLogin(); });
-    //Show login popup if not have publisher data
+//------- Core functions ---------
+
+//Function that render or update content of popup
+function RenderOrUpdatePopUpContent() {
+    //If not provided the needed data yet, show popup to register all needed data and return
     if (localStorage.getItem("publisherId") == null || localStorage.getItem("reviewsRss") == null) {
         document.getElementById("loginPopUpBgNode").setAttribute("style", "pointer-events: all; opacity: 0.5;");
         document.getElementById("loginPopUpBaseNode").setAttribute("style", "pointer-events: all; opacity: 1.0;");
+
+        //Create temporary publisher data, if not have yet
+        if (localStorage.getItem("temp_publisherId") == null)
+            localStorage.setItem("temp_publisherId", "");
+        if (localStorage.getItem("temp_reviewsRss") == null)
+            localStorage.setItem("temp_reviewsRss", "");
+
+        //Get fields
+        var publisherId = document.getElementById("loginPublisherId");
+        var reviewsRss = document.getElementById("loginReviewsRss");
+        publisherId.value = localStorage.getItem("temp_publisherId");
+        reviewsRss.value = localStorage.getItem("temp_reviewsRss");
+
+        //Save all typed on login fields on temporary storage
+        publisherId.addEventListener("input", function () {
+            localStorage.setItem("temp_publisherId", publisherId.value);
+        });
+        reviewsRss.addEventListener("input", function () {
+            localStorage.setItem("temp_reviewsRss", reviewsRss.value);
+        });
+
+        //Register listener to Save button
+        document.getElementById("loginButton").addEventListener("click", function () {
+            //Show errors
+            publisherId.style.borderColor = (publisherId.value == "") ? "red" : "";
+            reviewsRss.style.borderColor = (reviewsRss.value == "") ? "red" : "";
+
+            //If is empty, return
+            if (publisherId.value == "" || reviewsRss.value == "")
+                return;
+
+            //Save data
+            localStorage.setItem("publisherId", publisherId.value);
+            localStorage.setItem("reviewsRss", reviewsRss.value);
+            localStorage.removeItem("temp_publisherId");
+            localStorage.removeItem("temp_reviewsRss");
+
+            //Hide login popup
+            document.getElementById("loginPopUpBgNode").setAttribute("style", "pointer-events: none; opacity: 0;");
+            document.getElementById("loginPopUpBaseNode").setAttribute("style", "pointer-events: none; opacity: 0;");
+
+            //Force service to run a new update that will inform this popup on end
+            chrome.runtime.sendMessage({ msg: "OnRunNewUpdateOfService" });
+        });
+        return;
     }
 
-    console.log("updated");
-}
-
-//Validate and save the data typed in fields of login
-function ValidateAndSaveDataOfLogin() {
-    //Get fields
-    var publisherId = document.getElementById("loginPublisherId");
-    var reviewsRss = document.getElementById("loginReviewsRss");
-
-    //Show errors
-    publisherId.style.borderColor = (publisherId.value == "") ? "red" : "";
-    reviewsRss.style.borderColor = (reviewsRss.value == "") ? "red" : "";
-
-    //If is empty, return
-    if (publisherId.value == "" || reviewsRss.value == "")
+    //If not done loading of sales and reviews, return
+    if (doneLoadingSalesData == false || doneLoadingReviewsData == false)
         return;
 
-    //Save data
-    localStorage.setItem("publisherId", publisherId.value);
-    localStorage.setItem("reviewsRss", reviewsRss.value);
+    //Hide the loading screen
+    var loadingScreen = document.getElementById("loadingScreenNode");
+    loadingScreen.setAttribute("style", "opacity: 0; pointer-events: none;");
 
-    //Hide login popup
-    document.getElementById("loginPopUpBgNode").setAttribute("style", "pointer-events: none; opacity: 0;");
-    document.getElementById("loginPopUpBaseNode").setAttribute("style", "pointer-events: none; opacity: 0;");
+    //If have errors on load reviews or sales, show notification
+    if (haveErrorOnGetSalesInfo == true || haveErrorOnGetReviewsInfo == true) {
+        document.getElementById("getDataErrorMessage").setAttribute("style", "opacity: 1; display: block;");
+        document.getElementById("allOkMessage").style.opacity = "1";
+        setTimeout(function () {
+            document.getElementById("allOkMessage").style.display = "none";
+        }, 250);
+    }
+    //If not have errors on load reviews or sales, show notification
+    if (haveErrorOnGetSalesInfo == false && haveErrorOnGetReviewsInfo == false) {
+        document.getElementById("allOkMessage").setAttribute("style", "opacity: 1; display: block;");
+        document.getElementById("getDataErrorMessage").style.opacity = "0";
+        setTimeout(function () {
+            document.getElementById("getDataErrorMessage").style.display = "none";
+        }, 250);
+    }
+
+    //Register listener to publisher id changes
+    var prefsPublisherId = document.getElementById("prefsPublisherId");
+    prefsPublisherId.value = localStorage.getItem("publisherId");
+    var prefsReviewsRss = document.getElementById("prefsReviewsRss");
+    prefsReviewsRss.value = localStorage.getItem("reviewsRss");
+    var prefsPublisherButton = document.getElementById("prefsPublisherSave");
+    prefsPublisherButton.addEventListener("click", function () {
+        //Show errors
+        prefsPublisherId.style.borderColor = (prefsPublisherId.value == "") ? "red" : "";
+        prefsReviewsRss.style.borderColor = (prefsReviewsRss.value == "") ? "red" : "";
+
+        //If is empty, return
+        if (prefsPublisherId.value == "" || prefsReviewsRss.value == "")
+            return;
+
+        //Save data
+        localStorage.setItem("publisherId", prefsPublisherId.value);
+        localStorage.setItem("reviewsRss", prefsReviewsRss.value);
+    });
+
+    //Register listeners to preferences selects
+    var prefsDelayBetweenUpdates = document.getElementById("prefsDelayBetweenUpdates");
+    prefsDelayBetweenUpdates.value = localStorage.getItem("delayBetweenUpdates");
+    prefsDelayBetweenUpdates.addEventListener("change", function () {
+        localStorage.setItem("delayBetweenUpdates", prefsDelayBetweenUpdates.value);
+    });
+    var prefsNotifyOnStartService = document.getElementById("prefsNotifyOnStartService");
+    prefsNotifyOnStartService.value = localStorage.getItem("showNotificationOnStart");
+    prefsNotifyOnStartService.addEventListener("change", function () {
+        localStorage.setItem("showNotificationOnStart", prefsNotifyOnStartService.value);
+    });
+}
+
+//Function that report done loading sales data
+function ReportDoneLoadingSalesData(errorOcurred) {
+    //If already done, cancel report
+    if (doneLoadingSalesData == true)
+        return;
+
+    //Save the information
+    doneLoadingSalesData = true;
+    haveErrorOnGetSalesInfo = errorOcurred;
+
+    //Update the popup content
+    RenderOrUpdatePopUpContent();
+}
+//Function that report done loading reviews data
+function ReportDoneLoadingReviewsData(errorOcurred) {
+    //If already done, cancel report
+    if (doneLoadingReviewsData == true)
+        return;
+
+    //Save the information
+    doneLoadingReviewsData = true;
+    haveErrorOnGetReviewsInfo = errorOcurred;
+
+    //Update the popup content
+    RenderOrUpdatePopUpContent();
 }
